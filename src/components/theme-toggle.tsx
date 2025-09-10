8e77d8aaa85179754d8b7131ccc778b89d548b7e
ahsrafiq <ahsaanrafiq8@gmail.com>
2025-09-10 16:44:36 +0500

    Added monitor icon to show in the toggle

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -11,12 +11,23 @@ import {
 export function ThemeToggle() {
   const { setTheme, theme } = useTheme()
 
+  const renderIcon = () => {
+    switch (theme) {
+      case "light":
+        return <Sun className="h-[1.2rem] w-[1.2rem]" />
+      case "dark":
+        return <Moon className="h-[1.2rem] w-[1.2rem]" />
+      case "system":
+      default:
+        return <Monitor className="h-[1.2rem] w-[1.2rem]" />
+    }
+  }
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" size="sm" className="h-8 w-8 px-0">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          {renderIcon()}
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
